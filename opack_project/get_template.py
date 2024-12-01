b8b7f36b51468915af8546c70d174c0df83daaed
William Peltier <william.peltier33@gmail.com>
2024-12-01 15:42:36 +0100

    OpackApp

--- opack_project/get_template.py
+++
@@ -1,6 +0,0 @@
-from django.template.loader import get_template
-
-print(get_template('registration/password_reset.html'))
-print(get_template('registration/password_reset_done.html'))
-print(get_template('registration/password_reset_confirm.html'))
-print(get_template('registration/password_reset_complete.html'))
